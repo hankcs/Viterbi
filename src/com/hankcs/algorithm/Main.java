0118b6ef9a9d77d06718fd85ae76460b9c1ef880
hankcs <jfservice@126.com>
2014-09-10 20:31:15 +0800

    Initial commit

---
+++ src/com/hankcs/algorithm/Main.java
@@ -0,0 +1,40 @@
+package com.hankcs.algorithm;
+
+import static com.hankcs.algorithm.Main.Weather.*;
+import static com.hankcs.algorithm.Main.Activity.*;
+
+public class Main
+{
+    static enum Weather
+    {
+        Rainy,
+        Sunny,
+    }
+    static enum Activity
+    {
+        walk,
+        shop,
+        clean,
+    }
+    static int[] states = new int[]{Rainy.ordinal(), Sunny.ordinal()};
+    static int[] observations = new int[]{walk.ordinal(), shop.ordinal(), clean.ordinal()};
+    static double[] start_probability = new double[]{0.6, 0.4};
+    static double[][] transititon_probability = new double[][]{
+            {0.7, 0.3},
+            {0.4, 0.6},
+    };
+    static double[][] emission_probability = new double[][]{
+            {0.1, 0.4, 0.5},
+            {0.6, 0.3, 0.1},
+    };
+
+    public static void main(String[] args)
+    {
+        int[] result = Viterbi.compute(observations, states, start_probability, transititon_probability, emission_probability);
+        for (int r : result)
+        {
+            System.out.print(Weather.values()[r] + " ");
+        }
+        System.out.println();
+    }
+}
